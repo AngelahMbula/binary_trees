7c0c7873f249b68f5e57908b9cf5dee76ed7a1aa
AngelahMbula <mbula.angelah@gmail.com>
2022-11-16 10:03:46 -0800

    update

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -5,9 +5,9 @@
  * @func: pointer to a function to call each node
  * Return: void
  */
-void binary_tree_preorder(binary_tree_t *tree, void (*func)(int))
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL && func != NULL)
+	if (tree && func)
 	{
 		func(tree->n);
 		binary_tree_preorder(tree->left, func);
