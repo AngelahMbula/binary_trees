81861f447d367a5ccba2b9861510b6b07959e28e
AngelahMbula <mbula.angelah@gmail.com>
2022-11-17 03:13:29 -0800

    initial commit

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,8 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - measures the height of binary tree
+ * @tree: pointer to root node
+ * Return: height of binary tree,
+ * 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
