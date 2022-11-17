5d7a3a3ddd802d54cd1df6b8369b607d6faa6d0e
Angelah Mbula Maweu <104361772+AngelahMbula@users.noreply.github.com>
2022-11-17 14:19:33 +0300

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,3 +6,17 @@
  * 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
+{
+  int left_height = 0, right_height = 0;
+  
+  if (tree == NULL)
+        return (0);
+  else {
+        int left_height = binary_tree_height(tree->left);
+        int right_height = binary_tree_height(tree->right);
+        if (left_height >= right_height)
+            return left_height + 1;
+        else
+            return right_height + 1;
+    }
+}
