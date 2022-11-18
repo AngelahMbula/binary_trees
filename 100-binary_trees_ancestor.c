773eaf379bc228c50f8ecfdfe7b13344f1d94c8c
AngelahMbula <mbula.angelah@gmail.com>
2022-11-18 05:53:50 -0800

    initial commit

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,9 @@
+#include "binary_trees.h"
+/**
+ * binary_trees_ancestor - finds lowest common ancestor of two nodes
+ * @first: pointer to first node
+ * @second: pointer to second node
+ * Return: pointer to the lowest common ancestor
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
